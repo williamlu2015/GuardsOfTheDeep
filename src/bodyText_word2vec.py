65d15ff0ef90a90022449184ba480dccf3e070d5
William Lu <williamlu2015@alumni.ubc.ca>
2018-12-28 23:25:58 -0800

    Deleted unnecessary word2vec code

--- src/bodyText_word2vec.py
+++ src/bodyText_word2vec.py
@@ -10,36 +10,6 @@ from gensim.models import Word2Vec
 from gensim.utils import simple_preprocess
 
 
-class Corpus:
-    def __init__(self, filename):
-        self.filename = filename
-        self.file = None
-        self.csv_reader = None
-
-    def __enter__(self):
-        self.file = open("../dataset/dataset.csv", "r")
-        self.csv_reader = csv.DictReader(self.file, delimiter=",")
-        csv.field_size_limit(sys.maxsize)
-
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.file.close()
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        while True:
-            row = next(self.csv_reader)
-            body_text = row["bodyText"]
-
-            if body_text != "":
-                return body_text
-
-        # StopIteration exception is propagated
-
-
 def main():
     body_texts = _load_body_texts()
     corpus = _preprocess_body_texts(body_texts)
